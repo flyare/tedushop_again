95db69ef356b770ad3bf7a73c7ca413d34143806
DESKTOP-R3TBHPI\ducphan <itphanhoangduc@gmail.com>
2017-09-29 21:50:13 +0700

    Make model layer

---
+++ TeduShop.Model/Models/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Menus")]
+    public class Menu
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Url { get; set; }
+
+        public int? DisplayOrder { get; set; }
+
+        [Required]
+        public int GroupID { get; set; }
+
+        [ForeignKey("GroupID")]
+        public virtual MenuGroup MenuGroup { get; set; }
+        public string Target { get; set; }
+        public bool Status { get; set; }
+    }
+}
